be8e1ba7bc76572f88793e31418a4565ee11b935
Augusto Alonso <augustoalonso98@gmail.com>
2020-08-07 11:18:16 -0600

    finished gg

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ def get_signature(secret, message):
                     digestmod=hashlib.sha256).hexdigest().upper()
 
 
-print("UNSECURE SERVER VERIFICATION")
-# secretKey = 'CC3078'
-#
-# message = "Cifrado de informacion seccion 10"
-#
-#
-# print("INCISO 1.a")
-# print("Mensaje Cifrado de informacion seccion 10 ", get_signature(secretKey, message))
-# print("\n\n")
-# print("INCISO 1.b")
-# secretKey = 'MAC'
-#
-# message = "La implementacion de este ejercicio fue sencilla"
-#
-# print("La implementacion de este ejercicio fue sencilla ", get_signature(secretKey, message))
+secretKey = 'CC3078'
 
+message = "Cifrado de informacion seccion 10"
+
+
+print("INCISO 1.a")
+print("Mensaje Cifrado de informacion seccion 10 ", get_signature(secretKey, message))
+print("\n\n")
+print("INCISO 1.b")
+secretKey = 'MAC'
+
+message = "La implementacion de este ejercicio fue sencilla"
+
+print("La implementacion de este ejercicio fue sencilla ", get_signature(secretKey, message))
+
+print("INCISO 2")
 main()
 
